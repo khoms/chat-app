51ce283392650ab9ec6641cc43598b88d5890119
khoms <sundp.kshetri@gmail.com>
2023-07-30 11:30:15 +0545

    Start the server with a db connection

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
 
-app.listen(() => {
-  console.log("server is runnings");
-});
+const connectDB = require("./config/db");
+//Connect Db
+connectDB();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const ipAdd = process.env.IP_ADD;
+
+app.listen(PORT, console.log("Server running in " + ipAdd + ":" + PORT));
