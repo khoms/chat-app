246ed18f732b15cd372e8298a72fa9b24e131232
khoms <sundp.kshetri@gmail.com>
2023-08-04 17:17:08 +0545

    Remove the user from the active list after socket disconnect

--- packages/socket/socket.js
+++ packages/socket/socket.js
@@ -5,7 +5,7 @@ const io = require("socket.io")(8000, {
   },
 });
 
-const users = [];
+let users = [];
 
 const addUser = async (userId, socketId, userInfo) => {
   const checkUser = users.some((u) => u.userId === userId);
@@ -15,12 +15,21 @@ const addUser = async (userId, socketId, userInfo) => {
   }
 };
 
+const removeUser = async (socketId) => {
+  users = users.filter((u) => u.socketId !== socketId);
+};
+
 io.on("connection", (socket) => {
   console.log("Socket is connecting ...");
 
   socket.on("adduser", (userId, userInfo) => {
     addUser(userId, socket.id, userInfo);
 
+    io.emit("getUser", users);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
     io.emit("getUser", users);
   });
 });
